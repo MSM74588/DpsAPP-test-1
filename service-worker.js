a96530301f11e0fd533a530d8b4423c4ef563bfc
MandraSaptak Mandal <msm74588@gmial.com>
2021-05-22 09:58:09 +0530

    sw updated

--- service-worker.js
+++ service-worker.js
@@ -12,45 +12,63 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox
 // }
 
 workbox.routing.registerRoute(
-    ({request}) => request.destination === 'image',
+    ({ request }) => request.destination === 'image',
     new workbox.strategies.NetworkFirst()
 );
-
-
-
-
-
-self.addEventListener("fetch", event => {
-    console.log("You fetched " + event.url);
-});
-
 
 const staticCacheName = 'site-static';
 
 const assets = [
     '/',
-    '/index.html',
-    '/service-worker.js',
-    '/app.css',
-    '/manifest.webmanifest',
-    '/resources/menu.svg',
-    '/ico/arrow.svg',
-    '/ico/bookmark.svg',
-    '/ico/calender.svg',
-    '/ico/class.svg',
-    '/ico/qb.svg',
-    '/menu_page/menu.html',
-    '/menu_page/menu.css',
+    'index.html',
+    'service-worker.js',
+    'app.css',
+    'manifest.webmanifest',
+    'resources/menu.svg',
+    'ico/arrow.svg',
+    'ico/bookmark.svg',
+    'ico/calender.svg',
+    'ico/class.svg',
+    'ico/qb.svg',
+    'menu_page/menu.html',
+    'menu_page/menu.css',
     'https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap',
+    'https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js',
     // request urls here to store
 ];
 
 
-self.addEventListener('install'), evt => {
+
+
+
+
+
+
+// self.addEventListener("fetch", event => {
+//     console.log("You fetched " + event.url);
+// });
+
+
+
+
+self.addEventListener('install', evt => {
     evt.waitUntil(
         caches.open(staticCacheName).then(cache => {
-        cache.addAll(assets)
-        console.log('caching offline assets')
-    })
+            cache.addAll(assets);
+            console.log('caching offline assets')
+        })
+    );
+});
+
+self.addEventListener('fetch', evt => {
+    // console.log('fetch event',evt);
+    evt.respondWith(
+
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+
+        })
     )
-}
+});
+
+// https://youtu.be/g9LfyCZjeKI
